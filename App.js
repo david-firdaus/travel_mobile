18f41a4e2b43e0773cb522d90ef9ee556364ddbf
david.firdaus <dav.firdaus@gmail.com>
2017-11-20 23:14:18 +0700

    get login form action working, integrate authentication with firebase

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 import reducers from './src/reducers';
 import LoginForm from './src/components/LoginForm';
 
 export default class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyDiaTk7eng6iZGGo_1y5iliDe1bVL2R-ww',
+      authDomain: 'travels-2d85c.firebaseapp.com',
+      databaseURL: 'https://travels-2d85c.firebaseio.com',
+      projectId: 'travels-2d85c',
+      storageBucket: 'travels-2d85c.appspot.com',
+      messagingSenderId: '449563472385'
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <LoginForm />
       </Provider>
     );
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { Card, CardSection, Input, Button } from './common';
-import { emailChange } from '../actions';
+import { Card, CardSection, Input, Button, Spinner } from './common';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 
 
 class LoginForm extends Component {
     onEmailChange(text) {
-        this.props.emailChange(text);
+        this.props.emailChanged(text);
     }
 
     onPasswordChange(text) {
-        this.props.onPasswordChange(text);
+        this.props.passwordChanged(text);
+    }
+
+    onButtonPress() {
+        const { email, password } = this.props;
+        this.props.loginUser({ email, password });
+    }
+
+    renderError() {
+        if (this.props.error) {
+            return (
+                <View style={{ backgroundColor: 'white' }}>
+                    <Text style={styles.errorTextStyle}>{this.props.error}</Text>
+                </View>
+            );
+        }
+    }
+
+    renderButton() {
+        if (this.props.loading) {
+           return <Spinner size="large" />;
+        }
+        return (
+            <Button onPress={this.onButtonPress.bind(this)}>
+                Login
+            </Button>
+        );
     }
         
     render() {
@@ -31,24 +58,36 @@ class LoginForm extends Component {
                         placeholder='password'
                         onChangeText={this.onPasswordChange.bind(this)}
                         value={this.props.password}
+                        secureTextEntry
                     />
                 </CardSection>
+                {this.renderError()}
                 
                 <CardSection>
-                    <Button>
-                        Login
-                    </Button>
+                    {this.renderButton()}
                 </CardSection>
             </Card>
         );
     }
 }
 
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        color: 'red',
+        alignSelf: 'center'
+    }
+};
+
 const mapStateToProps = state => {
     return {
         email: state.auth.email,
-        password: state.auth.password
+        password: state.auth.password,
+        error: state.auth.error,
+        loading: state.auth.loading
     };
 };
 
-export default connect(mapStateToProps, emailChange)(LoginForm);
+export default connect(mapStateToProps, { 
+    emailChanged, passwordChanged, loginUser
+})(LoginForm);
